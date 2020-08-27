13960be61fa9870da5facfd5ac87037e49efb970
dejan-77 <dejankutic@me.com>
2020-08-28 00:05:59 +0200

    gfgf

--- barba003.js
+++ barba003.js
@@ -78,10 +78,9 @@ function init() {
     */ 
 
     const tl = gsap.timeline();
-        return tl.to(loader, {
-            duration: 0.3, scaleX: 0.1, /* squash the loader */ scaleY: 0.1, /* squash the loader */ /*yPercent: 0, /* move it down */ ease: 'Power4.inOut'
+        return tl.to(loader, {duration: 0.3, scale: 0, /* squash the loader *//* scaleY: 0.1,*/ /* squash the loader */ /*yPercent: 0, /* move it down */ ease: 'Power4.inOut'
         })
-            .fromTo([h1, p, img], { y:100, stagger:0.02, autoAlpha: 0 }, { duration: 0.3, autoAlpha: 1, stagger: 0.02, ease: 'none' }, 0.3);
+            .fromTo([h1, p, img], { y:100, autoAlpha: 0 }, { duration: 0.3, autoAlpha: 1, stagger: 0.02, ease: 'none' }, 0.3);
     }
 
     /* 
