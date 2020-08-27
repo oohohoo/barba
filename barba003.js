b8751fe1f4cb1fa948b57dc8d125fd2a823b3e59
dejan-77 <dejankutic@me.com>
2020-08-28 00:07:15 +0200

    eth

--- barba003.js
+++ barba003.js
@@ -80,7 +80,7 @@ function init() {
     const tl = gsap.timeline();
         return tl.to(loader, {duration: 0.3, scale: 0, /* squash the loader *//* scaleY: 0.1,*/ /* squash the loader */ /*yPercent: 0, /* move it down */ ease: 'Power4.inOut'
         })
-            .fromTo([h1, p, img], { y:100, autoAlpha: 0 }, { duration: 0.3, autoAlpha: 1, stagger: 0.02, ease: 'none' }, 0.3);
+            .fromTo([h1, p, img], { autoAlpha: 0 }, { duration: 0.3, autoAlpha: 1, stagger: 0.02, ease: 'none' }, 0.3);
     }
 
     /* 
