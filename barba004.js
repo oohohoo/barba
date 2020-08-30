4c9483c9893bf03fb780d73ef16ecda06e704d23
dejan-77 <dejankutic@me.com>
2020-08-30 14:55:18 +0200

    simple

--- barba004.js
+++ barba004.js
@@ -1,36 +1,16 @@
-function leaveAnimation(e) {
-
-    const elements = e.querySelector(".b-content");
-     return gsap
-      .to(elements, {
-        duration: 1,
-        rotate:-45,
-        y: 100,
-        opacity: 0,
-        ease: "power2.inOut",
-      })
-
- 
-}
-
-function enterAnimation(e) {
-
-    const elements = e.querySelector(".b-content");
-    return gsap
-   
-      .fromTo(elements, {y: 100, opacity: 0},
-        {duration: 1, y: 0, rotate:45, opacity: 1, ease: "power2.inOut"});
-
-}
-
+// SIMPLE VERSION from BARBA documentation
 barba.init({
-  debug: true,
-  transitions: [
-    {
-      sync: true,
-      leave: ({ current }) => leaveAnimation(current.container.querySelector(".main-inner")),
-      once: ({ next }) => enterAnimation(next.container.querySelector(".main-inner")),
-      enter: ({ next }) => enterAnimation(next.container.querySelector(".main-inner"))
-    }
-  ]
-});
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
