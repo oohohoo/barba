029f7265d5c50d2cd4e8b06663ea7f2c4047cc2b
dejan-77 <dejankutic@me.com>
2020-08-30 19:43:02 +0200

    synclonger

--- barba005.js
+++ barba005.js
@@ -5,7 +5,7 @@ barba.init({
         leave: (data) => {
             return new Promise(resolve => {
                 gsap.to(data.current.container, {
-                    duration:0.3, 
+                    duration:2, 
                     opacity: 0,
                     complete: () => {
                         resolve();
@@ -15,7 +15,7 @@ barba.init({
         },
         enter: (data) => {
             gsap.from(data.next.container, {
-                duration:0.3,
+                duration:2,
                 opacity: 0,
             });
         },
