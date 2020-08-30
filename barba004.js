8c9427393032c33840fb36bd00feb4875b409d1f
dejan-77 <dejankutic@me.com>
2020-08-30 15:17:03 +0200

    y100

--- barba004.js
+++ barba004.js
@@ -4,12 +4,12 @@ barba.init({
       name: 'opacity-transition',
       leave(data) {
         return gsap.to(data.current.container, {
-          opacity: 0
+          opacity: 0, y:100
         });
       },
       enter(data) {
         return gsap.from(data.next.container, {
-          opacity: 0
+          opacity: 0, y:100
         });
       }
     }]
