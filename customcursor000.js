d4eeea936fa3639a318cb14b0135eb9d7964dedb
dejan-77 <dejankutic@me.com>
2020-08-31 21:29:44 +0200

    fegm

--- customcursor000.js
+++ customcursor000.js
@@ -67,7 +67,7 @@ gsap.utils.toArray(".c-image-overlay").forEach(container => {
     tl
     /*.to(info, { yPercent: 20 })*/
       //.from(supscript, { ease: "power2.inOut", duration: 0.2, y: 20, autoAlpha:0, color: "#12335B", overwrite:"all" }, 0)
-     .to(".cursor", { scale:10, ease: "power2.inOut", duration: 0.3}, 0);
+     .to(".cursor", { scale:10, ease: "power2.inOut", duration: 0.3, overwrite:"all"});
     
     
     container.addEventListener("mouseenter", () => tl.play() );
