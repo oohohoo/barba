78e77b8349713410138d4e67d66c88365a19579d
dejan-77 <dejankutic@me.com>
2020-08-31 19:11:24 +0200

    nuovo

--- customcursor000.js
+++ customcursor000.js
@@ -1,5 +1,5 @@
 // GSAP QUICK SETTER
-gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+/* gsap.set(".cursor", {xPercent: -50, yPercent: -50});
 
 var ball = document.querySelector(".cursor");
 var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
@@ -21,6 +21,30 @@ gsap.ticker.add((time, deltaTime) => {
   
 	var delta = deltaTime * fpms;
 	var dt = 1.0 - Math.pow(1.0 - speed, delta); 
+  
+  pos.x += (mouse.x - pos.x) * dt;
+  pos.y += (mouse.y - pos.y) * dt;
+  xSet(pos.x);
+  ySet(pos.y);
+}); */
+
+gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+
+var ball = document.querySelector(".cursor");
+var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+var mouse = { x: pos.x, y: pos.y };
+var speed = 0.1;
+
+var xSet = gsap.quickSetter(ball, "x", "px");
+var ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {    
+  mouse.x = e.x;
+  mouse.y = e.y;  
+});
+
+gsap.ticker.add(() => {
+  var dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
   
   pos.x += (mouse.x - pos.x) * dt;
   pos.y += (mouse.y - pos.y) * dt;
