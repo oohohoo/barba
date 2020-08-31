f2bb658efe06a81a10482f02a10b70c2d3321f25
dejan-77 <dejankutic@me.com>
2020-08-31 21:18:44 +0200

    dgh

--- customcursor000.js
+++ customcursor000.js
@@ -60,14 +60,14 @@ gsap.ticker.add((time, deltaTime) => {
 gsap.utils.toArray(".c-image-overlay").forEach(container => {
     let 
     /*info = container.querySelector(".information"),*/
-      test = container.querySelector(".test"),
+     // test = container.querySelector(".test"),
    
         tl = gsap.timeline({ paused: true });
     
     tl
     /*.to(info, { yPercent: 20 })*/
       //.from(supscript, { ease: "power2.inOut", duration: 0.2, y: 20, autoAlpha:0, color: "#12335B", overwrite:"all" }, 0)
-     .to(test, { scale:4, ease: "power2.inOut", duration: 0.3, autoAlpha:0.7}, 0);
+     .to(".test", { scale:4, ease: "power2.inOut", duration: 0.3, autoAlpha:0.7}, 0);
     
     
     container.addEventListener("mouseenter", () => tl.play() );
