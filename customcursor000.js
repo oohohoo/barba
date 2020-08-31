22e9a8ce4072fa8554f921916d40aa738ae45224
dejan-77 <dejankutic@me.com>
2020-08-31 18:54:48 +0200

    first comit

---
+++ customcursor000.js
@@ -0,0 +1,28 @@
+// GSAP QUICK SETTER
+
+var ball = document.querySelector(".cursor");
+var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+var mouse = { x: pos.x, y: pos.y };
+var speed = 0.1;
+
+var fpms = 60 / 1000;
+
+var xSet = gsap.quickSetter(ball, "x", "px");
+var ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {    
+  mouse.x = e.x;
+  mouse.y = e.y;  
+
+});
+
+gsap.ticker.add((time, deltaTime) => {
+  
+	var delta = deltaTime * fpms;
+	var dt = 1.0 - Math.pow(1.0 - speed, delta); 
+  
+  pos.x += (mouse.x - pos.x) * dt;
+  pos.y += (mouse.y - pos.y) * dt;
+  xSet(pos.x);
+  ySet(pos.y);
+});
