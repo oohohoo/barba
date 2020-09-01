845ff39fe8c79c1ffd0282c73daad9153872f325
dejan-77 <dejankutic@me.com>
2020-09-01 05:04:04 +0200

    dhj

--- customcursor000.js
+++ customcursor000.js
@@ -99,32 +99,3 @@ document.querySelector('.cursor').style.backgroundImage = "url(https://res.cloud
     })
 	});
 	
-
-
-
-
-
-
-
-
-
-
-
-/* 
-// GSAP UTILS TO ARRAY ----- kao radi ali sa zamuckivanjem
-gsap.utils.toArray(".c-image-overlay").forEach(container => {
-    let 
-   //info = container.querySelector(".information"),
-     // test = container.querySelector(".test"),
-   
-        tl = gsap.timeline({ paused: true });
-    
-    tl
-   //.to(info, { yPercent: 20 })
-      //.from(supscript, { ease: "power2.inOut", duration: 0.2, y: 20, autoAlpha:0, color: "#12335B", overwrite:"all" }, 0)
-     .to(ball, { scale:10, ease: "power2.inOut", duration: 0.3, overwrite:"all"});
-    
-    
-    container.addEventListener("mouseenter", () => tl.play() );
-    container.addEventListener("mouseleave", () => tl.reverse() );
-  }); */
