b3d6bce7c68cb98d665b94de1ca72f94735d71d1
dejan-77 <dejankutic@me.com>
2020-09-01 17:28:04 +0200

    locowork

--- barba/barba004.js
+++ barba/barba004.js
@@ -1,29 +1,3 @@
-// SIMPLE VERSION / FADEOUT -> FADEIN // from SLACK
-
-barba.init({
-    transitions: [{
-       name: 'legacy-example',
-    leave: function(data) {
-      var done = this.async();
-      gsap.to(data.current.container, {
-        duration:0.3, 
-        opacity: 0,
-        onComplete: done
-      });
-    },
-    enter: function(data) {
-      gsap.from(data.next.container, {
-        duration:0.3,
-        opacity: 0,
-        onComplete: () => {
-          this.async();
-        }
-      });
-    }
-  }]
-});
-
-
 /*
   ================================================================================
     SMOOTH SCROLL + SCROLLTRIGGER
@@ -65,3 +39,69 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
+
+
+// SIMPLE VERSION / FADEOUT -> FADEIN // from SLACK
+// vidi da li?
+let locoScroll;
+
+barba.init({
+  transitions: [{
+    sync: true,
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    },
+    once({ next }) {
+      initScroll(next.container);
+    },
+    beforeEnter({ next }) {
+      locoScroll.destroy();
+      initScroll(next.container);
+    }
+  }]
+});
+
+function initScroll(container) {
+  if (container.hasAttribute('data-scroll-container')) {
+    locoScroll = new LocomotiveScroll({
+      el: container,
+      smooth: true
+    });
+  }
+}
+
+
+// ---- PRVA VERZIJA
+/* 
+barba.init({
+  transitions: [{
+    sync: true, // ---- check
+     name: 'legacy-example',
+  
+  leave: function(data) {
+    var done = this.async();
+    gsap.to(data.current.container, {
+      duration:0.3, 
+      opacity: 0,
+      onComplete: done
+    });
+  },
+  enter: function(data) {
+    gsap.from(data.next.container, {
+      duration:0.3,
+      opacity: 0,
+      onComplete: () => {
+        this.async();
+      }
+    });
+  }
+}]
+}); */
