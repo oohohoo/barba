d679734eae03d750e096b94a2334140e833dada9
dejan-77 <dejankutic@me.com>
2020-12-27 18:25:16 +0100

    bhbvh

--- barba/main.js
+++ barba/main.js
@@ -187,7 +187,7 @@ function initLocomotiveScroll() {
   });
 
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+//ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
