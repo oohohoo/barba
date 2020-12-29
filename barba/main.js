585046e59623bb57499e54c07aebcc83e1448afb
dejan-77 <dejankutic@me.com>
2020-12-29 07:46:28 +0100

    dd

--- barba/main.js
+++ barba/main.js
@@ -506,7 +506,7 @@ function animationEnter() {
   
   tl
    //.from(navlink, {duration: 0.6, autoAlpha:0, yPercent:100, stagger:0.2, ease: 'power1.out'})
-   .fromTo(underline, {scaleX:0},{duration: 0.6, scaleX:1, ease: 'power1.out'})
+   .fromTo(underline, {scaleX:0},{duration: 0.6, scaleX:1, ease: 'power1.out'}, 0.5)
      //.from(mask, {xPercent:-101},0)
      //.from(homeimg, {xPercent:101},0);
      console.log("underline");
