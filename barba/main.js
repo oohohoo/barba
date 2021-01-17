ab50cc6f39467264c74a7395bc08050e87425fcc
dejan-77 <dejankutic@me.com>
2021-01-17 11:52:40 +0100

    gr

--- barba/main.js
+++ barba/main.js
@@ -383,7 +383,7 @@ BARBA TRANSITIONS
         animationEnter();
         customCursor();
         //homeanimations();
-        console.log("ONCE +++ JAO!");
+        console.log("ONCE +++ BLA!");
      },
 
      
