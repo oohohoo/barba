817f8ba4d057a51fce160d621bf6dc382e25b327
dejan-77 <dejankutic@me.com>
2021-01-17 18:39:04 +0100

    fheu

--- barba/main.js
+++ barba/main.js
@@ -384,7 +384,7 @@ BARBA TRANSITIONS
         animationEnter();
         customCursor();
         //homeanimations();
-        console.log("ONCE +++ žaofao!");
+        console.log("ONCE +++ žaofaoo!");
      },
 
 
