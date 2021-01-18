149d1e329e6ad661324630ae7504573b01765216
dejan-77 <dejankutic@me.com>
2021-01-18 09:55:04 +0100

    jhjj

--- barba/main.js
+++ barba/main.js
@@ -384,7 +384,7 @@ BARBA TRANSITIONS
         animationEnter();
         customCursor();
         //homeanimations();
-        console.log("ONCE +++ uhihi!");
+        console.log("ONCE +++ ghghgh");
      },
 
      
